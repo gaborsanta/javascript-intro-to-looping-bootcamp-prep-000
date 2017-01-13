ba176cd4f08b77a6e77de5887cc8b40b7b46c338
gabor santa <gabor.santa@gmail.com>
2017-01-13 16:22:23 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,17 +1,17 @@
 
+function forLoop(array) {
 
+  for (var i = 0; i < 25; i++) {
 
-function doWhileLoop(array) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
 
-  function maybeTrue() {
-    return Math.random() >= 0.5
   }
+  return array
 
-    do {
-      array = array.slice(1)
-    } while (array.length > 0 && maybeTrue());
-
-    return array
 }
 
 
@@ -28,18 +28,15 @@ function whileLoop(i) {
 
 }
 
+function doWhileLoop(array) {
 
-function forLoop(array) {
-
-  for (var i = 0; i < 25; i++) {
-
-    if (i === 1) {
-      array.push("I am 1 strange loop.")
-    } else {
-      array.push(`I am ${i} strange loops.`)
-    }
-
+  function maybeTrue() {
+    return Math.random() >= 0.5
   }
-  return array
 
+    do {
+      array = array.slice(1)
+    } while (array.length > 0 && maybeTrue());
+
+    return array
 }
